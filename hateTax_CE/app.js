9a225a25b364556e35a20c4d1c7eaab730a972ec
Pablo Sotres <contact@pablosotres.com>
2021-04-14 13:33:45 -0500

    orevio a la reunion

--- hateTax_CE/app.js
+++ hateTax_CE/app.js
@@ -1,17 +1,6 @@
-var mode = "";
-var status ="";
-var automatic ="";
-var randomTime;
-var timerInterval;
+replaceText(document.body)
 
-/**********************
-*	General Constants
-***********************/
-
-
-/**********************
-*	Search And interact
-***********************/
+var numero = Math.round(Math.random() * 25);
 
 function writingTheTimePrice(){
 
@@ -19,117 +8,25 @@ function writingTheTimePrice(){
 	// var salarioMinino = 120;
 	// var horasLaborales = 8;
 	// var salarioxhora = 120/8;
-	descuento =Math.random()*25;
+	descuento =Math.round(Math.random()*25)/100;
+    console.log(descuento)
 
-	var amount = document.getElementsByClassName("price-tag-fract")[0];
-	if(amount != null){
-		clearInterval(timerInterval);
-		var amount_items = document.getElementsByClassName("amount");
-		for(amount_item in amount_items){
-
-				var timePriceHolder = amount_items[amount_item];
-				var timePrice = amount_items[amount_item].innerHTML;
-
-				timePrice = timePrice.replace("<small>$</small>","")
-				console.log(timePrice)
-				timePrice = parseInt(timePrice);
-
-				var valorTiempo  = timePrice/salarioxhora
-				valorTiempo = valorTiempo.toFixed(2);
-				console.log("salario por hora: "+valorTiempo)
-				timePriceHolder.innerHTML = String(valorTiempo)+"<small> horas</small>";
-
-
-		}
-
-
-	}
-
-
-	var price_item = document.getElementsByClassName("a-product__paragraphDiscountPrice")[0];
-	if (price_item != null ) {
-
-		console.log("salario por hora: "+salarioxhora)
-		var timePrice = document.getElementsByClassName("a-product__paragraphDiscountPrice")[0].innerHTML;
-		timePrice = timePrice.replace("$","")
-		timePrice = timePrice.replace(",","")
-		timePrice = timePrice.replace('<sup class="undefined">00</sup>',"")
-		timePrice = timePrice.replace('<sup class="undefined">00</sup>',"")
-		timePrice = timePrice.replace('<!-- -->',"")
-
-		timePrice = parseInt(timePrice);
-
-		var valorTiempo  = timePrice/salarioxhora
-
-		valorTiempo = valorTiempo.toFixed(2);
-
-		console.log("salario por hora: "+valorTiempo)
-
-
-		console.log(valorTiempo);
-
-		document.getElementsByClassName("a-product__paragraphDiscountPrice")[0].innerHTML = String(valorTiempo)+" horas" ;
-		clearInterval(timerInterval);
-	}
-	var prices_list = document.getElementsByClassName("a-card-discount")[0];
-	if(prices_list != null){
-		var prices = document.getElementsByClassName("a-card-discount");
-		console.log(prices_list.length)
-		for( var price in prices){
-
-			try{
-				var timePriceHolder = prices[price];
-				var timePrice = prices[price].innerHTML;
-				timePrice = timePrice.replace("$","")
-				timePrice = timePrice.replace(",","")
-				timePrice = timePrice.replace('<sup class="undefined">00</sup>',"")
-				timePrice = timePrice.replace('<sup class="undefined">00</sup>',"")
-				timePrice = timePrice.replace('<!-- -->',"")
-				timePrice = parseInt(timePrice);
-
-				var valorTiempo  = timePrice/salarioxhora
-
-				valorTiempo = valorTiempo.toFixed(2);
-				console.log(timePriceHolder)
-
-				timePriceHolder.innerHTML = String(valorTiempo)+" horas" ;
-			}catch(e){
-
-			}
-
-			clearInterval(timerInterval);
-
-		}
-
-
-	}
-
+    var amount_items = document.querySelectorAll("span.price-tag-fraction")
+    
+    for(amount_item in amount_items){
+        // console.log(amount_item)
+        var cantidadHolder = amount_items[amount_item];
+        var cantidad = amount_items[amount_item].innerHTML;
+        // console.log(cantidad)
+        if(cantidad.includes('')){
+            cantidad = cantidad.replace(/,/g, '')
+        }
+        cantidad =parseInt(cantidad)
+        cantidad = Math.round((cantidad *descuento)+cantidad)
+        
+        
+        cantidadHolder.innerHTML = String(cantidad);
+    }
+    
 
 }
-
-/**********************
-*	Working / Init
-***********************/
-
-function working(){
-
-	//console.log(':::::::::::: Extension: Working ::::::::::::');
-
-	timerInterval = setInterval(writingTheTimePrice, 1000);
-
-	//console.log(":::::::::::: Extension: Writing Header Info ::::::::::::");
-
-	//setTimeout(function(){
-	//		console.log(":::::::::::: Extension: Time reached / Seeking ::::::::::::");
-	//	searchNInteract();
-	//}, randomTime);
-}
-
-/**********************
-*	On load / Extension status
-***********************/
-
-(function(){
-
-	working();
-})();
---
+++ hateTax_CE/popup.js
@@ -0,0 +1,3 @@
+var cambiar = document.getElementById('resultado')
+var descuento = Math.round(Math.random()*25)/100 
+cambiar.innerHTML = descuento*100 + "%"
